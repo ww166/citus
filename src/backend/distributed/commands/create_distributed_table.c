d2707bb784d93465ae0d5af8093acdbe020553c9
Marco Slot <marco.slot@gmail.com>
2021-06-18 14:31:56 +0200

    Allow unsafe triggers via a GUC

--- src/backend/distributed/commands/create_distributed_table.c
+++ src/backend/distributed/commands/create_distributed_table.c
@@ -1354,6 +1354,12 @@ EnsureTableNotDistributed(Oid relationId)
 static void
 EnsureRelationHasNoTriggers(Oid relationId)
 {
+	if (EnableUnsafeTriggers)
+	{
+		/* user really wants triggers */
+		return;
+	}
+
 	List *explicitTriggerIds = GetExplicitTriggerIdList(relationId);
 
 	if (list_length(explicitTriggerIds) > 0)
