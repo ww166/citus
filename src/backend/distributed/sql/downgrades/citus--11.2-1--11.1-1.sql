7c8b46582b000f289ad75eda785da75480260902
Nils Dijk <nils@citusdata.com>
2022-12-12 14:36:42 +0100

    drop database objects with regards to shardgroups

--- src/backend/distributed/sql/downgrades/citus--11.2-1--11.1-1.sql
+++ src/backend/distributed/sql/downgrades/citus--11.2-1--11.1-1.sql
@@ -23,3 +23,14 @@ COMMENT ON FUNCTION pg_catalog.worker_append_table_to_shard(text, text, text, in
 
 #include "../udfs/worker_split_shard_replication_setup/11.1-1.sql"
 DROP FUNCTION pg_catalog.citus_task_wait(bigint, pg_catalog.citus_task_status);
+
+DROP TABLE pg_catalog.pg_dist_shardgroup;
+DROP SEQUENCE citus.pg_dist_shardgroupid_seq;
+
+ALTER TABLE pg_catalog.pg_dist_shard DROP COLUMN shardgroupid;
+
+DROP FUNCTION pg_catalog.citus_internal_add_shard_metadata(regclass, bigint, "char", text, text, bigint);
+#include "udfs/citus_internal_add_shard_metadata/10.2-1.sql"
+
+DROP FUNCTION pg_catalog.citus_internal_add_shardgroup_metadata(bigint, integer, text, text);
+DROP FUNCTION FUNCTION pg_catalog.citus_internal_delete_shardgroup_metadata(bigint, int);
